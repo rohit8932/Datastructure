764517af85cac468a43327e202dae721f65edfaf
Rohit <rohitrajak8932@gmail.com>
2016-12-25 07:54:37 +0530

    [Stack]Program 8: infix to postfix (with parenthesis) using stack

---
+++ stack_8.cpp
@@ -0,0 +1,85 @@
+// infix to postfix (with parenthesis )using stack
+
+#include<iostream>
+#include<stack>
+#include<cstdio>
+#include<string.h>
+using namespace std;
+bool checkOperand(char a){
+    if(a>='a'&&a<='z')
+        return true;
+        else if(a>='0' && a<='9')
+            return true;
+    return false;
+}
+
+bool isOperator(char a){
+    if(a=='+' || a=='-' || a=='*' || a=='/')
+        return true;
+    return false;
+}
+
+bool isHighprec(char top, char curr){
+    if(top=='*' && curr=='+') return true;
+    else if (top=='*' && curr =='-') return true;
+    else if (top=='/' && curr =='+') return true;
+    else if (top=='/' && curr =='-') return true;
+
+    return false;
+}
+void infixTopostfix(char a[]){
+    stack<char> s;
+    char result[20]="";                     // very important. it initializes array with empty
+    int j=0;
+    for(int i=0;i<strlen(a);i++){
+        if(checkOperand(a[i])){
+            result[j++]=a[i];
+            cout <<result<<endl;
+
+        }
+        else if(isOperator(a[i])){
+                if(!s.empty() && isHighprec(s.top(),a[i]) && s.top()!='('){
+
+                    result[j++]=s.top();
+                    s.pop();
+                    cout<< "curr > top "<<endl;
+
+                }
+                s.push(a[i]);
+
+
+        }
+
+        else if(a[i]=='('){
+                    s.push(a[i]);
+                }
+        else if(a[i]==')'){
+            while(!s.empty()&&s.top()!='('){
+                    result[j++]=s.top();
+                    s.pop();
+                  }
+                  s.pop(); // this is for removing opening bracket ie. (
+
+        }
+
+    }
+    while(!s.empty()){
+        result[j++]=s.top();
+        s.pop();
+
+    }
+
+    cout<< result;
+
+
+}
+
+int main(){
+    char a[20];
+    cout <<"Enter your Expression\n";
+    fgets(a,20,stdin);
+    infixTopostfix(a);
+
+
+    return 0;
+}
